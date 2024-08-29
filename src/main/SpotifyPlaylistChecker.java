0a6c959df1eac064697c738bb8de4e57b0c304d1
Lenny <Leonardobelli2006@gmail.com>
2024-08-29 04:19:49 +0200

    check playlist id and playlist link

---
+++ src/main/SpotifyPlaylistChecker.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SpotifyPlaylistChecker {
+
+	private static final String URL_PATTERN = "https:\\/\\/open\\.spotify\\.com\\/playlist\\/[a-zA-Z0-9]+(\\?.*)?";
+    private static final String URI_PATTERN = "spotify:playlist:[a-zA-Z0-9]+";
+    private static final String PLAYLIST_ID_PATTERN = "^[a-zA-Z0-9]{22}$";
+
+    public static boolean isSpotifyPlaylistId(String id) {
+        Pattern pattern = Pattern.compile(PLAYLIST_ID_PATTERN);
+        Matcher matcher = pattern.matcher(id);
+        return matcher.matches();
+    }
+
+    public static boolean isSpotifyPlaylist(String link) {
+        Pattern urlPattern = Pattern.compile(URL_PATTERN);
+        Pattern uriPattern = Pattern.compile(URI_PATTERN);
+
+        Matcher urlMatcher = urlPattern.matcher(link);
+        Matcher uriMatcher = uriPattern.matcher(link);
+
+        return urlMatcher.matches() || uriMatcher.matches();
+    }
+    
+}
